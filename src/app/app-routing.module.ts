1c14cc451974a489b1b4bfdf36887904ff83b077
chamaboy <hiroponpoko07@gmail.com>
2020-10-10 10:55:53 +0900

    feat(routing):各画面、Routing設計 2h #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'init-form',
+    loadChildren: () =>
+      import('./init-form/init-form.module').then((m) => m.InitFormModule),
+  },
+  {
+    path: 'quize',
+    loadChildren: () =>
+      import('./quize/quize.module').then((m) => m.QuizeModule),
+  },
+  {
+    path: 'settings',
+    loadChildren: () =>
+      import('./settings/settings.module').then((m) => m.SettingsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/init-form/init-form-routing.module.ts
+++ src/app/init-form/init-form-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitFormComponent } from './init-form/init-form.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InitFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class InitFormRoutingModule { }
+export class InitFormRoutingModule {}
